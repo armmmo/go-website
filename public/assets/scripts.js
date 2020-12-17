df68e2e7f15415279b6d065ac49e47b19784fc5e
armando manuel moura de oliveira <armandomouraoliveira@gmail.com>
2020-12-17 09:38:38 -0300

    header

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -1,5 +1,4 @@
 
-<script>
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function() {
         var currentScrollPos = window.pageYOffset;
@@ -18,4 +17,31 @@
         prevScrollpos = currentScrollPos;
     } 
     
-</script>
+
+    function openPlanBW2020(elementIdToShow, idScrollTo) {
+        var i;
+        var tabs = document.getElementsByClassName("pricing-table-tab-content");
+        var buttons = document.getElementsByClassName("pricing-table-tab-button");
+        var mobileInputText = document.getElementById("pricing-table-tab-button-text");
+        var mobileInputTextAb = document.getElementById("pricing-table-tab-button-text-ab");
+
+        for (i = 0; i < tabs.length; i++) {
+            tabs[i].id === elementIdToShow ?
+                tabs[i].classList.remove('d-none') :
+                tabs[i].classList.add('d-none');
+        }
+        for (i = 0; i < buttons.length; i++) {
+            if (buttons[i].dataset.id === elementIdToShow) {
+                if (idScrollTo === 'pricing-table-scroll-to-new-cw-ab') {
+                    mobileInputTextAb.innerHTML = buttons[i].innerText;
+                } else {
+                    mobileInputText.innerHTML = buttons[i].innerText;
+                }
+                buttons[i].classList.add('bg-red');
+            } else {
+                buttons[i].classList.remove('bg-red');
+            }
+        }
+        this.scrollToTargetWithNavigationOffset(idScrollTo, -20)
+    }
+
