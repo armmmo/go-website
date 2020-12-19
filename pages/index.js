5537631b1506077940a8828a70ea633eb6af48ce
armando manuel moura de oliveira <armandomouraoliveira@gmail.com>
2020-12-19 00:22:44 -0300

    hhh

--- pages/index.js
+++ pages/index.js
@@ -961,6 +961,9 @@ export default function Home() {
             </div>
           </div> 
         </footer> 
+
+        <div className="box-whatsapp">
+        </div>
         <a href="https://api.whatsapp.com/send?phone=5511960373269&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." className="float" target="_blank">
           <i className="fa fa-whatsapp my-float"></i>
         </a>
