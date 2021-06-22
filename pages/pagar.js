b2dcc3d196f1c369f9291fc54f105b96ab69bc90
armando manuel moura de oliveira <armandomouraoliveira@gmail.com>
2021-06-22 20:58:55 -0300

    fix

--- pages/pagar.js
+++ pages/pagar.js
@@ -1,31 +1,34 @@
 import React from 'react'
 
-export default function pagar() {
-    openCheckout() {
-      let checkout = new PagarMeCheckout.Checkout({
-        encryption_key: "ek_test_QL5ckR2Gj8LgmmO3G1MnOAph8UrpAD",
-        success: function(data) {
-          alert(JSON.stringify(data));
-        },
-        error: function(err) {
-          alert(JSON.stringify(err));
-        },
-        close: function() {
-          alert("The modal has been closed.");
-        }
-      });
-  
-      checkout.open({
-        amount: 8000,
-        buttonText: "Pagar",
-        customerData: "true",
-        createToken: "false",
-        paymentMethods: "credit_card"
-      });
+import React from 'react';
+
+export default function componentName() {
+
+    openCheckout = () => {
+        let checkout = new PagarMeCheckout.Checkout({
+          encryption_key: "ek_test_QL5ckR2Gj8LgmmO3G1MnOAph8UrpAD",
+          success: function(data) {
+            alert(JSON.stringify(data));
+          },
+          error: function(err) {
+            alert(JSON.stringify(err));
+          },
+          close: function() {
+            alert("The modal has been closed.");
+          }
+        });
+    
+        checkout.open({
+          amount: 8000,
+          buttonText: "Pagar",
+          customerData: "true",
+          createToken: "false",
+          paymentMethods: "credit_card"
+        });
     }
-  
-    render() {
-      return <button onClick={this.openCheckout}>Abrir o Checkout</button>;
-    }
+
+  return (
+    <button onClick={this.openCheckout}>Abrir o Checkout</button>
+  );
 }
   
