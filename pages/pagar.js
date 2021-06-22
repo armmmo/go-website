5f37e763fd340f4b0cb241b3a5eca427b4df7279
armando manuel moura de oliveira <armandomouraoliveira@gmail.com>
2021-06-22 20:28:46 -0300

    pagar

---
+++ pages/pagar.js
@@ -0,0 +1,30 @@
+class App extends React.Component {
+    openCheckout() {
+      let checkout = new PagarMeCheckout.Checkout({
+        encryption_key: "ek_test_QL5ckR2Gj8LgmmO3G1MnOAph8UrpAD",
+        success: function(data) {
+          alert(JSON.stringify(data));
+        },
+        error: function(err) {
+          alert(JSON.stringify(err));
+        },
+        close: function() {
+          alert("The modal has been closed.");
+        }
+      });
+  
+      checkout.open({
+        amount: 8000,
+        buttonText: "Pagar",
+        customerData: "true",
+        createToken: "false",
+        paymentMethods: "credit_card"
+      });
+    }
+  
+    render() {
+      return <button onClick={this.openCheckout}>Abrir o Checkout</button>;
+    }
+  }
+  
+  ReactDOM.render(<App />, document.getElementById("react-container"));
